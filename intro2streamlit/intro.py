8647394b5b5b98e8029ff4fdc7c7567a722568b7
Garrett Speed <6378547+gspeed0689@users.noreply.github.com>
2025-03-06 09:28:15 +0100

    File path correction test

--- intro2streamlit/intro.py
+++ intro2streamlit/intro.py
@@ -8,7 +8,7 @@ st.set_page_config(
 
 st.title("Welcome to the Programming Cafe Introduction to Streamlit!")
 
-with open("assets/intro_markdown_01.md", "r") as f:
+with open("../assets/intro_markdown_01.md", "r") as f:
     markdown_01 = f.read()
 
 st.markdown(markdown_01)
