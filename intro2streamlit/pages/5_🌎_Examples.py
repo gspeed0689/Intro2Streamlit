067aba28204e20fe4f67d807d66495255ba6652c
Garrett Speed <6378547+gspeed0689@users.noreply.github.com>
2025-03-06 09:57:56 +0100

    More examples

--- intro2streamlit/pages/5_🌎_Examples.py
+++ intro2streamlit/pages/5_🌎_Examples.py
@@ -14,4 +14,35 @@ The app uses many of the tools on the previous pages to explore what I created.
 st.markdown(vesuvius_md)
 
 st.link_button("Go to the Vesuvius Panorama Example", "https://vesuvius.garrettspeed.com/")
+st.link_button("View Vesuvius Source Code", "https://github.com/gspeed0689/lavalit")
+
+geobenchmarks_md = """
+## GIS Benchmarks
+
+Not currently running, however It Works On My Machine™, and you can see the source code: 
+An app for uploading GIS benchmark data and get immediate results visualizations. 
+
+This app works with the output files of the ArcGIS Pro ProPAT benchmarking tool and has three components:
+
+1. The FastAPI web server to interface with a database
+2. The upload Streamlit app which has a file upload input, the app then parses the input file, creates a Pandas dataframe, displays that to the uploader for approval, then POSTs that to the FastAPI endpoint.
+3. The viewer Streamlit app will query the FastAPI endpoint for data, build a Pandas dataframe, and then display the results with options for grouping by computer or storage system. 
+"""
+
+st.markdown(geobenchmarks_md)
+
+st.link_button("View the upload app source code", "https://github.com/gspeed0689/propat-benchmark-viewer/tree/master/upload_app")
+st.link_button("View the viewer app source code", "https://github.com/gspeed0689/propat-benchmark-viewer/blob/master/viewer_app/viewer.py")
+
+audio_md = """
+## Audio Converter
+
+For some researchers in a remote location, I made a locally deployable Streamlit app that would accept a file upload and convert that file to the lowest quality MP3 setting. 
+The goal of this is to reduce file sizes for upload to a cloud service, large file sizes were experiencing errors with the spotty remote internet connection.
+
+"""
+
+st.markdown(audio_md)
+
+st.link_button("View the overview video", "https://youtu.be/DvkIuB-6pw0")
 
